a73ad61a29849451b889584212f63f8c4b65c720
gabiprds93m <gabiprds.93@gmail.com>
2018-03-20 17:36:37 -0500

    Mejorando header

--- src/header.js
+++ src/header.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import './header.css';
 import {NavLink} from 'react-router-dom';
+import logo from './images/logo.png';
+
 export const Header = ({cart}) => 
 {
-    return (
-        <div id="mar-header">
-            <h1 className="logo"><a href="#/"></a></h1>
-            <div className="btn-header">
-                <NavLink to="/" id="cart-info"> Shopping Cart
-                    {/* <a id="cart-info" href="#">Shopping Cart */}
-                        <span><span data-bind="text: cart.contentsCount">{cart.length}</span> items</span>
-                    {/* </a> */}
-                </NavLink>
+    return(
+        <div>
+            <div className="row" id="header">
+                <div className="col-lg-2 col-lg-offset-1 col-md-3 col-md-offset-1 col-sm-4 col-sm-offset-1 col-xs-4">
+                    <NavLink to="/"> 
+                        <img src={logo}/>
+                    </NavLink>
+                </div>
+                <div className="col-lg-3 col-lg-offset-5 col-md-5 col-sm-5 col-xs-4">
+                    <div className="btn-header">
+                        <NavLink to="/" id="cart-info"> 
+                            Shopping Cart
+                            <span><span data-bind="text: cart.contentsCount">{cart.length}</span> items</span>
+                        </NavLink>
+                    </div>
+                </div>
             </div>
-        </div>
+            <div className="row" id="header-line">
+                <div className="col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10">
+                </div>
+            </div>
+        </div>        
     )
 }
 
